f371269b45842e36bd2bd3d4e4b247d7b7e2eeea
Maria1982 <britosmarialuz@gmail.com>
2023-04-23 18:55:38 -0300

    Inicializacion del proyecto en Spring Boot

---
+++ california-back/california-back/src/main/java/com/example/californiaback/CaliforniaBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.californiaback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaliforniaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaliforniaBackApplication.class, args);
+	}
+
+}
---
+++ california-back/california-back/src/test/java/com/example/californiaback/CaliforniaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.californiaback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaliforniaBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
